dd2c82a7aea1b7de9f3993a489272dc2d8d7756c
Anshu Krishna <anshu.krishna5@gmail.com>
2022-02-24 14:29:27 +0530

    UUID added

---
+++ src/UUID.php
@@ -0,0 +1,18 @@
+<?php
+namespace Krishna\Utilities;
+
+final class UUID {
+	use StaticOnlyTrait;
+
+	public static function gen(bool $use_com = false) {
+		if($use_com) {
+			try {
+				return trim(com_create_guid(), "{}");
+			} catch (\Throwable $th) {}
+		}
+		$data = random_bytes(16);
+		$data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+		$data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));	
+	}
+}
